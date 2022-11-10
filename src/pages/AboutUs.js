700da96f62053336b495f69a61a4ceb43beb7274
bassil <bassiljarrah2@gmail.com>
2022-11-10 18:32:38 +0300

    4.Faq Section

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -2,12 +2,14 @@ import React from "react";
 //import componnents 
 import AboutSection from "../components/AboutSection";
 import ServesSection from "../components/ServesSection";
+import FaqSection from "../components/FaqSection";
 
 const AboutUs=()=>{
   return(
     <>
      <AboutSection />
      <ServesSection />
+     <FaqSection />
     </>
   );
 }
